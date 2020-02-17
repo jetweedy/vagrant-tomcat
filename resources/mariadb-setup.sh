0235baeeb58e35a92590510c45379518e249e9c1
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-02-17 15:20:24 -0500

    Add files via upload

--- resources/mariadb-setup.sh
+++ resources/mariadb-setup.sh
@@ -2,8 +2,8 @@
 
 # http://tomcat.apache.org/tomcat-7.0-doc/jndi-datasource-examples-howto.html
 sudo cp /vagrant/mysql-connector-java-8.0.19.jar /opt/tomcat/latest/lib/mysql-connector-java-8.0.19.jar
-sudo apt-get install -y mysql-client
-sudo apt-get install -y libmysql-java
+#sudo apt-get install -y mysql-client
+#sudo apt-get install -y libmysql-java
 
 ### INSTALL MSQL
 export DEBIAN_FRONTEND=noninteractive
