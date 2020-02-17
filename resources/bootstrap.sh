00c1e7f84eb89c90a834e82984fe8d06e14859c0
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-02-17 13:43:57 -0500

    Add files via upload

--- resources/bootstrap.sh
+++ resources/bootstrap.sh
@@ -6,15 +6,5 @@ sudo apt-get install zip -y
 sudo apt-get install unzip -y
 sudo apt-get install dos2unix -y
 
-#sudo dos2unix /vagrant/mariadb-setup.sh
-#sudo dos2unix /vagrant/node-setup.sh
-#sudo dos2unix /vagrant/apache-setup.sh
-#sudo dos2unix /vagrant/lamp-setup.sh
-#sudo dos2unix /vagrant/python-setup.sh
-#sudo dos2unix /vagrant/react-setup.sh
-
-#sudo bash /vagrant/mariadb-setup.sh
-#sudo bash /vagrant/node-setup.sh
-#sudo bash /vagrant/apache-setup.sh
-#sudo bash /vagrant/lamp-setup.sh
-#sudo bash /vagrant/python-setup.sh
+sudo dos2unix /vagrant/tomcat-setup.sh
+sudo bash /vagrant/tomcat-setup.sh
